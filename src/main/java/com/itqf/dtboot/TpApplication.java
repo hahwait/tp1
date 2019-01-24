f2fea5f7dde7d4d13e25ca578a6f8309bfded237
hahwait <635141119@qq.com>
2019-01-24 10:20:59 +0800

    环境提交

---
+++ src/main/java/com/itqf/dtboot/TpApplication.java
@@ -0,0 +1,14 @@
+package com.itqf.dtboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpApplication.class, args);
+    }
+
+}
+
